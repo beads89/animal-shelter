87bedbf3121877256e0535b3eb1aecad0be560ea
Brandon Eads <brandon181989@hotmail.com>
2021-10-29 08:43:35 -0700

    continue building initial files, add controllers/models

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -14,46 +14,79 @@ using Microsoft.OpenApi.Models;
 
 namespace AnimalShelter
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelter", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
-}
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+      services.AddApiVersioning(x =>
+      {
+              //x.DefaultApiVersion = new ApiVersion(1, 0);
+              x.AssumeDefaultVersionWhenUnspecified = true;
+        x.ReportApiVersions = true;
+              //x.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
+            });
+
+
+      services.AddSwaggerGen(options =>
+      {
+        options.SwaggerDoc("v1",
+                  new Microsoft.OpenApi.Models.OpenApiInfo
+              {
+                Title = "Animal Shelter",
+                Description = "Documentation Provided by Swagger",
+                Version = "v1"
+              });
+
+        var fileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+        options.IncludeXmlComments(filePath);
+
+        options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+      });
+
+      // services.AddApiVersioning(o => {
+      // o.ReportApiVersions = true;
+      // o.AssumeDefaultVersionWhenUnspecified = true;
+      // o.DefaultApiVersion = new ApiVersion(1, 0);
+      // });
+
+      services.AddDbContext<AnimalShelterContext>(opt =>
+          opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+
+      app.UseSwagger();
+
+      app.UseSwaggerUI(options =>
+      {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Animal Shelter API");
+              // options.RoutePrefix = "";
+            });
+    }
+  }
